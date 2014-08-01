76199066f0f197a0446367dc2633fa1c29b912bc
Diego Balseiro (MBF) <diego.balseiro@syntagma.com.ar>
2014-08-01 17:31:10 -0300

    Linea de comando

--- MembershipProject/Program.cs
+++ MembershipProject/Program.cs
@@ -10,26 +10,93 @@ namespace MembershipProject
     class Program
     {
         private static ItemAction action;
+        private static bool interactive = true;
 
         static void Main(string[] args)
         {
-            while (true)
+            if (args != null && args.Length > 0)
             {
-                printMenu();
+                interactive = false;
                 try
                 {
-                    selectAction();
-                    if (action == ItemAction.EXIT) break;
-                    ActionFactory.create(action).doAction();
+                    handleParams(args);
+                    doAction();
                 }
-                catch (Exception ex)
+                catch (Exception e)
                 {
-                    Console.WriteLine(ex.Message);
-                    Console.Write("Presione ENTER para continuar . . .");
-                    Console.ReadLine();
-                    Console.WriteLine();
+                    handleException(e);
                 }
             }
+            else
+            {
+                interactive = true;
+                while (true)
+                {
+                    printMenu();
+                    try
+                    {
+                        selectAction();
+                        if (action == ItemAction.EXIT) break;
+                        doAction();
+                    }
+                    catch (Exception ex)
+                    {
+                        handleException(ex);
+                    }
+                }
+            }
+        }
+
+        private static void doAction()
+        {
+            if (action == ItemAction.EXIT) return;
+            ActionFactory.create(action).doAction();
+        }
+
+        private static void handleException(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            if (interactive)
+            {
+                Console.Write("Presione ENTER para continuar . . .");
+                Console.ReadLine();
+                Console.WriteLine();
+            }
+        }
+
+        private static void handleParams(string[] args)
+        {
+            try
+            {
+                if (args.Length != 1) throw new ArgumentException();
+                string sarg = args[0];
+                if (sarg == "?")
+                {
+                    usage();
+                    action = ItemAction.EXIT;
+                    return;
+                }
+                int arg = int.Parse(sarg);
+                if (arg < 1 || arg > Enum.GetValues(typeof(ItemAction)).Length - 1)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                parseSelection(sarg);
+            }
+            catch (Exception e)
+            {
+                usage();
+                throw e;
+            }
+        }
+
+        private static void usage()
+        {
+            Console.WriteLine("USO: membership [arg]");
+            Console.WriteLine("Si Arg es vacio se muestra programa interactivo");
+            Console.WriteLine("Si arg es ? se muestra esta ayuda");
+            Console.WriteLine("o puede ser alguna de las siguientes opciones");
+            printMenu();
         }
 
         private static void printMenu()
@@ -46,9 +113,13 @@ namespace MembershipProject
             Console.Write("Seleccione una opcion: ");
             string r = Console.ReadLine();
             Console.WriteLine();
+            parseSelection(r);
+        }
+
+        private static void parseSelection(string r)
+        {
             int seleccion = int.Parse(r);
             action = (ItemAction)Enum.GetValues(typeof(ItemAction)).GetValue(seleccion);
         }
-
     }
 }
