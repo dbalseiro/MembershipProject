cdbde77fbe33482b95c90925ab4537d36c97157c
Diego Balseiro <diego.balseiro@syntagma.com.ar>
2014-08-17 22:32:54 -0300

    version web

---
+++ MembershipWeb/Models/Parameters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MembershipWeb.Models
+{
+    public class Parameters : List<Parameter>
+    {
+        public string nombre { get; set; }
+    }
+
+    public class Parameter
+    {
+        public string text { get; set; }
+        public string value { get; set; }
+    }
+}
---
+++ MembershipWeb/Scripts/events.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function() {
+    $("a.submit").click(function(event) {
+        event.preventDefault();
+        var formId = $(this).attr("data-formid");
+        formId && $("form#" + formId).submit();        
+    });
+});
