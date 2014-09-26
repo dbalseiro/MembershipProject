6a6730f04efa56ea5b9179a2fe2b66c9ff316a75
Diego Balseiro (MBF) <diego.balseiro@syntagma.com.ar>
2014-09-26 18:34:52 -0300

    reseteo de TODOS los usuarios

--- MembershipCLI/ResetUsersWithoutAnswer.bat
+++ MembershipCLI/ResetUsersWithoutAnswer.bat
@@ -1,30 +1,9 @@
 @echo off
 
-for /F %%i in ('MembershipCLI.lnk 2') do (
-	echo USER %%i:
-	MembershipCLI.lnk 1 %%i
+for /F %%j in (2 3) do (
+	for /F %%i in ('MembershipCLI.lnk %%j') do (
+		echo USER %%i:
+		MembershipCLI.lnk 1 %%i
+	)
 )
 
-' USER 20272390445:
-' Kjgx6414
-' USER 20182075265:
-
-' Shro5023
-' USER 33710220099:
-
-' Qmea4808
-' USER 20072893876:
-
-' Dofa4128
-
-' USER 20085010930:
-' Oplh5424
-
-' USER 20122181171:
-' Rxoj4485
-
-' USER 30641870311:
-' Xvfg4372
-
-' USER 20103934088:
-' Qlvh5801
